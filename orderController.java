ef59844f4af950093f74850c2b0ea6024aee6fbe
JonCattuna <jcattuna@gmail.com>
2020-11-03 19:16:34 -0500

    Add files via upload

---
+++ orderController.java
@@ -0,0 +1,51 @@
+import java.awt.event.ActionEvent;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class orderController {
+
+    @FXML
+    private ListView<?> Checkout;
+
+    @FXML
+    private Button ClearButton;
+
+    @FXML
+    private Button DupeButton;
+
+    @FXML
+    private Button removeButton;
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private TextField finalPrice;
+
+    @FXML
+    private Button saveOrder;
+
+    @FXML
+    void ClearCart(ActionEvent event) {
+
+    }
+
+    @FXML
+    void DupeOrder(ActionEvent event) {
+
+    }
+
+    @FXML
+    void backToMenu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void exportOrder(ActionEvent event) {
+
+    }
+
+}
